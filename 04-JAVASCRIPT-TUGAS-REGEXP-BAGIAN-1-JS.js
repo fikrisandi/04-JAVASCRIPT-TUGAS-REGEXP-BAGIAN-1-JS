06abcf8a5322e2c130362e8ae745f0d7e27f1c62
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-23 21:44:23 +0700

    Update 04-JAVASCRIPT-TUGAS-REGEXP-BAGIAN-1-JS.js

--- 04-JAVASCRIPT-TUGAS-REGEXP-BAGIAN-1-JS.js
+++ 04-JAVASCRIPT-TUGAS-REGEXP-BAGIAN-1-JS.js
@@ -1,12 +1,11 @@
 function panggilRegexp(value) {
-    let str = "abcdefghijklmnopqrstuvwxyz-A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z";
-    var a = str.match(/[cC]/g)
-    var b = str.match(/[kK]/g)
-    var c = str.match(/[lL]/g)
+ let str = "abcdefghijklmnopqrstuvwxyz-A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z";
 
-    console.log(a);
-    console.log(b);
-    console.log(c);
+ //Tulis Code Jawaban Kamu Di Bawah ini
+ console.log(str.match(/c/ig));
+ console.log(str.match(/k/ig));
+ console.log(str.match(/l/ig));
+
 }
 
 panggilRegexp()
